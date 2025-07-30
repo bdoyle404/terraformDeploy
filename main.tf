29a9adea8fdfaaf822bd857ea24ccec658946517
Billy Doyle <djodoyle@gmail.com>
2025-07-30 08:45:24 -0500

    Further modifications to main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,74 @@
 ## See https://github.com/tariq87/terraformDeploy/tree/main
-## Must create a VPC with the name "vpc_name" before running this script.
-## Must create a VPC Subnet associated with "vpc_name" before running this script.
+## Creates the VPC and subnet and uses their IDs.
 
-variable "region" {}
-variable "vpc_name" {}
+variable "region" {
+    default = "us-east-2"
+}
+variable "basename" {
+    default = "terraformDeploy"
+}
+variable "vpc_name" {
+    default = "vpn-terraformDeploy"
+}
+variable "subnet_name" {
+    default = "snet-terraformDeploy"
+}
 
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
+# --- Create VPC and subnet -----------------------------------------------
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = var.vpc_name
+    Purpose = "test"
+  }
+}
+
+resource "aws_subnet" "subnet_id" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.0.0/25" # inside 10.0.0.0/24
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.subnet_name}"
+    Purpose = "test"
+  }
+}
+
+# --- AMI lookup -----------------------------------------------------------
 data "aws_ami" "latest_amazon_linux" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*"]
-    }
-    filter {
-        name = "architecture"
-        values = ["x86_64"]
-    }
-    filter {
-        name = "root-device-type"
-        values = ["ebs"]
-    }
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 }
 
-data "aws_vpc" "selected_vpc" {
-    filter {
-        name   = "tag:Name"
-        values = ["${var.vpc_name}"]
-    }
-}
-
-data "aws_subnets" "private" {
-    filter {
-        name   = "vpc-id"
-        values = [data.aws_vpc.selected_vpc.id]
-    }
-    # filter {
-    #     name   = "tag:Name"
-    #     values = ["${var.vpc_name}"]
-    # }
-
-}
-
-
+# --- Launch instance in the created subnet --------------------------------
 resource "aws_instance" "ec2_instance" {
-    ami           = data.aws_ami.latest_amazon_linux.id
-    instance_type = "t2.micro"
-    subnet_id     = data.aws_subnets.private.ids[0]
-    tags = {
-        Name = "WebServer"
-        Purpose = "test"
-        Stack = "terraformDeploy"
-    }
+  ami           = data.aws_ami.latest_amazon_linux.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet_id.id
+
+  tags = {
+    Name    = "WebServer"
+    Purpose = "test"
+    Stack   = "${var.basename}"
+  }
 }
